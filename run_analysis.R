af6afe2d1764454577839b48eb4d995a61b4d370
Krokodim <dmitry.grekov@gmail.com>
2015-02-15 03:11:54 +0300

    hello world

---
+++ run_analysis.R
@@ -0,0 +1,56 @@
+#library(dplyr)
+#library(data.table)
+library(reshape2)
+
+
+# read the column names
+  features <- read.table("UCI HAR Dataset/features.txt", col.names=c("id","caption"), stringsAsFactors=FALSE)
+
+# exclude unnecessary columns, i.e.
+# leave only those containing mean() or std() 
+  features <- features[grep(".*mean\\(\\)|.*std\\(\\)", features[,2]),]
+
+# enhance the column names
+  f <- function(s_del, s_ins) features$caption <<- gsub(s_del, s_ins, features$caption)
+  f("^t", "time-")
+  f("^f", "frequency-")
+  f("Acc", "Accelerometer")
+  f("Gyro", "Gyroscope")
+  f("Mag", "Magnitude")
+  f("BodyBody", "Body")
+  f("mean()", "MEAN")
+  f("std()", "STD")
+  rm(f)
+
+# read the train data and select only columns
+# with numbers from features$id
+  x.train <- read.table("UCI HAR Dataset/train/X_train.txt")[, features$id]
+
+# set column names 
+  colnames(x.train) <- features$caption
+
+#read and set activity & subject
+  x.train <- cbind(activity = read.table("UCI HAR Dataset/train/y_train.txt",col.names="activity"), x.train)
+  x.train <- cbind(subject  = read.table("UCI HAR Dataset/train/subject_train.txt",col.names="subject"), x.train)
+
+# do the same with the test data
+  x.test <- read.table("UCI HAR Dataset/test/X_Test.txt")[, features$id]
+  colnames(x.test) <- features$caption
+  x.test <- cbind(activity = read.table("UCI HAR Dataset/test/y_test.txt",col.names="activity"), x.test)
+  x.test <- cbind(subject  = read.table("UCI HAR Dataset/test/subject_test.txt",col.names="subject"), x.test)
+
+# merge the data sets
+  x.full <- rbind(x.train, x.test)
+
+# convert activity codes into factors
+  activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors=TRUE )
+  x.full$activity <- factor(x.full$activity, levels = activity.labels[,1], labels = activity.labels[,2])
+
+# melt the data frame 
+  molten.x.full <- melt(x.full, id = c('subject', 'activity'))
+
+# and cast it calculating means 
+  x.agg <- dcast(molten.x.full, subject + activity ~ variable, mean)
+
+# cleanup
+  rm(list=setdiff(ls(),"x.agg"))
